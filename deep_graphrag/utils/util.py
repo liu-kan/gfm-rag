618ff006d9fc0e491702805dc57510dd70115f07
RManLuo <34737002+RManLuo@users.noreply.github.com>
2024-11-06 15:17:19 +1100

    feat: support multi KG pretraining

--- deep_graphrag/utils/util.py
+++ deep_graphrag/utils/util.py
@@ -3,6 +3,9 @@ import os
 
 import torch
 from hydra.utils import get_class
+from omegaconf import DictConfig
+
+from deep_graphrag.datasets import KGDataset
 
 
 def load_model_from_pretrained(path: str) -> tuple[torch.nn.Module, dict]:
@@ -12,3 +15,16 @@ def load_model_from_pretrained(path: str) -> tuple[torch.nn.Module, dict]:
     state = torch.load(os.path.join(path, "model.pth"), map_location="cpu")
     model.load_state_dict(state["model"])
     return model, config
+
+
+def build_pretrain_dataset(cfg: DictConfig) -> list[KGDataset]:
+    """
+    Return the joint KG datasets
+    """
+    dataset_cfg = {k: v for k, v in cfg.datasets.items() if k != "data_names"}
+    data_name_list = cfg.datasets.data_names
+    dataset_list = []
+    for data_name in data_name_list:
+        kg_data = KGDataset(**dataset_cfg, data_name=data_name)[0]
+        dataset_list.append(kg_data)
+    return dataset_list
