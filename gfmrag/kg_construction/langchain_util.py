186907a6613215e549c3b3e856be1a7ae2473df0
Raymond Luo <34737002+RManLuo@users.noreply.github.com>
2025-03-05 21:00:54 +0800

    chore: format code

--- gfmrag/kg_construction/langchain_util.py
+++ gfmrag/kg_construction/langchain_util.py
@@ -2,9 +2,9 @@ import os
 from typing import Any
 
 from langchain_community.chat_models import ChatLlamaCpp, ChatOllama
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
 from langchain_openai import ChatOpenAI
 from langchain_together import ChatTogether
-from langchain_nvidia_ai_endpoints import ChatNVIDIA
 
 
 def init_langchain_model(
@@ -32,14 +32,16 @@ def init_langchain_model(
             timeout=timeout,
             **kwargs,
         )
-    elif llm  == "nvidia":
+    elif llm == "nvidia":
         # https://python.langchain.com/docs/integrations/chat/nvidia_ai_endpoints/
 
-        return ChatNVIDIA(nvidia_api_key=os.environ.get("NVIDIA_API_KEY"),
-                          base_url="https://integrate.api.nvidia.com/v1",
-                          model=model_name,
-                          temperature=temperature,
-                          **kwargs)
+        return ChatNVIDIA(
+            nvidia_api_key=os.environ.get("NVIDIA_API_KEY"),
+            base_url="https://integrate.api.nvidia.com/v1",
+            model=model_name,
+            temperature=temperature,
+            **kwargs,
+        )
     elif llm == "together":
         # https://python.langchain.com/v0.1/docs/integrations/chat/together/
 
